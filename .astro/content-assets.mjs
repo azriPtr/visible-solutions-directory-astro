adb62f9c63b688dad0d45c61ddec949a675696e8
azriPtr <azriputraor@gmail.com>
2026-01-19 01:22:00 +0800

    .

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_ZMGkKo from "../../assets/article-2.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fhigh-security-locks.md";
-import __ASTRO_IMAGE_IMPORT_Z6qxON from "../../assets/article-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Flocksmith-cost-2026.md";
 import __ASTRO_IMAGE_IMPORT_1S06Ng from "../../assets/article-3.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fupvc-door-handle-stuck.md";
-export default new Map([["../../assets/article-2.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fhigh-security-locks.md", __ASTRO_IMAGE_IMPORT_ZMGkKo], ["../../assets/article-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Flocksmith-cost-2026.md", __ASTRO_IMAGE_IMPORT_Z6qxON], ["../../assets/article-3.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fupvc-door-handle-stuck.md", __ASTRO_IMAGE_IMPORT_1S06Ng]]);
+import __ASTRO_IMAGE_IMPORT_Z6qxON from "../../assets/article-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Flocksmith-cost-2026.md";
+export default new Map([["../../assets/article-2.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fhigh-security-locks.md", __ASTRO_IMAGE_IMPORT_ZMGkKo], ["../../assets/article-3.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fupvc-door-handle-stuck.md", __ASTRO_IMAGE_IMPORT_1S06Ng], ["../../assets/article-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Flocksmith-cost-2026.md", __ASTRO_IMAGE_IMPORT_Z6qxON]]);
 		
